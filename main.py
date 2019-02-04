68857f5372e8676e0ac8ab4df664d4d3a7b63621
kazo2 <47306787+kazo2@users.noreply.github.com>
2019-02-04 10:30:36 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,45 +1,49 @@
 import random
 import time
 
+rps = ['rock','paper','scissors']
+
 def game():
-    print('Choose rock, paper, or scissors.')
-    choice = input()
+    print('\nChoose rock, paper, or scissors.')
+    choice = input().lower()
 
-    if choice.lower() in ('rock','paper','scissors'):
-        AI_choice = random.choice(['Rock','Paper','Scissors'])
-        print(f'You chose {choice.lower()} and I chose {AI_choice.lower()}')
+    if choice in rps:
+        AI_choice = random.choice(rps)
+        print(f'You chose {choice} and I chose {AI_choice}')
         time.sleep(1)
-        if choice.lower() == 'rock':
-            if AI_choice == 'Rock':
+        if choice == 'rock':
+            if AI_choice == 'rock':
                 print("Rats! It's a tie.")
-            elif AI_choice == 'Paper':
+                game()
+            elif AI_choice == 'paper':
                 print("Woo-hoo! Looks like I won.")
             else:
                 print("Well played, looks like you won.")
 
-        elif choice.lower() == 'paper':
-            if AI_choice == 'Rock':
+        elif choice == 'paper':
+            if AI_choice == 'rock':
                 print("Woo-hoo! Looks like I won.")
-            elif AI_choice == 'Paper':
+            elif AI_choice == 'paper':
                 print("Rats! It's a tie.")
+                game()
             else:
                 print("Well played, looks like you won.")
         else:
-            if AI_choice == 'Rock':
+            if AI_choice == 'rock':
                 print("Woo-hoo! Looks like I won.")
-            elif AI_choice == 'Paper':
+            elif AI_choice == 'paper':
                 print("Well played, looks like you won.")
             else:
                 print("Rats! It's a tie.")
-        time.sleep(3)
-        print('That was fun! Wanna play again?\nY/N')
-        playAgain = input()
-        if playAgain.lower() in ('y','yes','sure'):
+                game()
+        time.sleep(2)
+        print('\nThat was fun! Wanna play again?\nY/N')
+        while True:
+            playAgain = input().lower()
+            if playAgain not in ('y','yes','sure','ok','okay'):
+                break
             game()
-        else:
-            exit()
     else:
-        print('Please type in either rock, paper, or scissors.\n')
-        time.sleep(1)
+        print('\n' * 15 + 'Please type in either rock, paper, or scissors.')
         game()
 game()
