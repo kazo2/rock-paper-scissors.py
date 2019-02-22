d9faaabc84c50bc76beb53e0b6b4670799fc3aea
kazo2 <47306787+kazo2@users.noreply.github.com>
2019-02-22 14:15:51 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,49 +1,34 @@
 import random
 import time
 
-rps = ['rock','paper','scissors']
 
 def game():
     print('\nChoose rock, paper, or scissors.')
     choice = input().lower()
 
-    if choice in rps:
-        AI_choice = random.choice(rps)
+    if choice in ['rock', 'paper', 'scissors']:
+        AI_choice = random.choice(['rock', 'paper', 'scissors'])
         print(f'You chose {choice} and I chose {AI_choice}')
         time.sleep(1)
-        if choice == 'rock':
-            if AI_choice == 'rock':
-                print("Rats! It's a tie.")
-                game()
-            elif AI_choice == 'paper':
-                print("Woo-hoo! Looks like I won.")
-            else:
-                print("Well played, looks like you won.")
-
-        elif choice == 'paper':
-            if AI_choice == 'rock':
-                print("Woo-hoo! Looks like I won.")
-            elif AI_choice == 'paper':
-                print("Rats! It's a tie.")
-                game()
-            else:
-                print("Well played, looks like you won.")
-        else:
-            if AI_choice == 'rock':
-                print("Woo-hoo! Looks like I won.")
-            elif AI_choice == 'paper':
-                print("Well played, looks like you won.")
-            else:
-                print("Rats! It's a tie.")
-                game()
-        time.sleep(2)
+        if choice == 'rock' and AI_choice == 'scissors' or choice == 'paper' and AI_choice == 'rock' or choice == 'scissors' and AI_choice == 'paper':
+            print("Well played, looks like you won.")
+            time.sleep(2)
+        elif choice == 'paper' and AI_choice == 'scissors' or choice == 'scissors' and AI_choice == 'rock' or choice == 'rock' and AI_choice == 'paper':
+            print("Woo-hoo! Looks like I won.")
+            time.sleep(2)
+        elif choice == 'scissors' and AI_choice == 'scissors' or choice == 'rock' and AI_choice == 'rock' or choice == 'paper' and AI_choice == 'paper':
+            print("Rats! It's a tie.")
+            time.sleep(2)
         print('\nThat was fun! Wanna play again?\nY/N')
         while True:
             playAgain = input().lower()
-            if playAgain not in ('y','yes','sure','ok','okay'):
+            if playAgain not in ('y', 'yes', 'sure', 'ok', 'okay'):
                 break
             game()
     else:
-        print('\n' * 15 + 'Please type in either rock, paper, or scissors.')
+        print('\nPlease type in either rock, paper, or scissors.')
+        time.sleep(1)
         game()
+
+
 game()
